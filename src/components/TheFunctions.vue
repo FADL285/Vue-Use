19995d5dc245fc076bf77cb17e49fa26673030b2
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2022-11-22 17:31:36 +0200

    Initial proj & use useTitle function

---
+++ src/components/TheFunctions.vue
@@ -0,0 +1,27 @@
+<script setup>
+import FunctionItem from "./FunctionItem.vue";
+import UseTitle from "./functions/UseTitle.vue";
+</script>
+
+<template>
+  <FunctionItem>
+    <template #heading>
+      <a href="https://vueuse.org/core/usetitle/#usetitle" target="_blank">
+        UseTitle
+      </a>
+    </template>
+
+    <UseTitle />
+  </FunctionItem>
+
+  <FunctionItem>
+    <template #heading>Tooling</template>
+
+    This project is served and bundled with
+  </FunctionItem>
+  <FunctionItem>
+    <template #heading>Ecosystem</template>
+
+    Get official tools and libraries for your project:
+  </FunctionItem>
+</template>
---
+++ src/components/functions/UseTitle.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useTitle } from "@vueuse/core";
+
+const title = useTitle();
+console.log(title.value); // print current title
+title.value = "Hello VueUse"; // change current title
+</script>
+
+<template>
+  <div>
+    <label for="title">Page Title</label>
+    <input type="text" name="title" id="title" v-model="title" />
+  </div>
+</template>
